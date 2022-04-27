b0c696f702bf54294ad35acee5f0932374f3e148
Jared Lai <jl.jared.jl@gmail.com>
2022-04-27 00:40:32 -0700

    Addedd comments

--- image_downloader.go
+++ image_downloader.go
@@ -15,12 +15,16 @@ import (
 
 var wg sync.WaitGroup
 
+// helper funciton to check for errors. 
 func errorCheck(err error) {
 	if err != nil {
 		fmt.Print(err.Error())
 	}
 }
 
+// saves image to the output folder
+// takes two inputs, file_name and the url for the image
+// downloads iamge to the output folder, creates one if folder doesn't already exist
 func save_image(file_name, img_url string) error {
 
 	// first parse the end of the url to get the extention type of the file
@@ -34,6 +38,7 @@ func save_image(file_name, img_url string) error {
 	file_name += "."
 	file_name += splitted[len(splitted)-1]
 
+	// get the image from the given url
 	response, err := http.Get(img_url)
 	errorCheck(err)
 
@@ -65,15 +70,19 @@ func save_image(file_name, img_url string) error {
 		return err
 	}
 
-	wg.Done()
+	wg.Done()	// tell wait group this goroutine is finished
 	return nil
 }
 
+// downloads images concurrently 
+// given a map of houses, loops througha and downloads images
+// does not return a value 
 func Download_Images(houses map[string]string) {
 
+	// looping through the elements in the map
 	for file_name, img_url := range houses {
-		wg.Add(1)
-		go save_image(file_name, img_url)
+		wg.Add(1)	// adds to the wait group
+		go save_image(file_name, img_url)	// calls save_image concurrently 
 	}
-	wg.Wait()
+	wg.Wait()	// waits for all goroutines to finish 
 }
--- response_handler.go
+++ response_handler.go
@@ -14,22 +14,28 @@ import (
 
 var api_key = "http://app-homevision-staging.herokuapp.com/api_project/houses/"
 
+// structs for the json response
 type EntireResponse struct {
 	House []House 	`json:"houses"`
 	Ok bool `json:"ok"`
 }
 
+// struct for each house, extracting info needed 
 type House struct {
 	Id int `json:"id"`
 	Address string `json:"address"`
 	URL string `json:"photoURL"`
 }
 
+// function that sends a get request and returns the json data
+// takes two parameters, page and per_page, which are the url params for the api
 func GetResponse(page int, per_page string) ([]byte) {
 
+	// format the url
 	url := api_key + "?page=" + strconv.FormatInt(int64(page), 10) + "&per_page=" + per_page
 	var data []byte
 
+	// loop to keep sending get requests to the api in case of falilure (since it's flaky)
 	for {
 		response, err := http.Get(url)
 
@@ -37,6 +43,9 @@ func GetResponse(page int, per_page string) ([]byte) {
 			fmt.Print(err.Error())
 			os.Exit(1)
 		}
+
+		// check if the response is valid(aka statuscode = 200)
+		// if true break and return 
 		if response.StatusCode == 200 {
 
 			responseData, err := ioutil.ReadAll(response.Body)
@@ -52,15 +61,20 @@ func GetResponse(page int, per_page string) ([]byte) {
 	return data
 }
 
+// funciton that parses the json
+// takes one parameter data which should be the json data
+// returns a map with the house id-address as the key and the image url as the value
 func Parse_json(data []byte) (map[string]string) {
+	// declare variables
 	house_map := make (map[string]string)
 	
 	var responseObject EntireResponse
-	json.Unmarshal(data, &responseObject)
+	json.Unmarshal(data, &responseObject)	// parse the json
 
+	// loop through all the houses 
 	for _, house := range responseObject.House {
 		// fmt.Println(house.Id, house.Address, house.URL)
-		house_map[fmt.Sprint(house.Id) + "-" + house.Address] = house.URL
+		house_map[fmt.Sprint(house.Id) + "-" + house.Address] = house.URL	// store data into the map 
 	}
 	return house_map
 }
